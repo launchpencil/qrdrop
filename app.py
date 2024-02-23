59fc1f8f83cc6dca2de919a8c7c4e32773175b23
launchpencil <launchpencil@gmail.com>
2024-02-23 20:28:41 +0900

    完成

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 import streamlit as st
 from minio import Minio
+import os, uuid, qrcode, io
 
-client = Minio("minio.launchpencil.f5.si",
-    access_key="Q3AM3UQ867SPQQA43P2F",
-    secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+client = Minio(
+    "minio.launchpencil.f5.si",
+    access_key=os.environ.get("accesskey"),
+    secret_key=os.environ.get("secretkey"),
 )
 
-st.title('hello world!')
+st.title('QRdrop v1.0')
+st.caption("選択されたファイルからQRコードを生成します。")
 
-source_file = "requirements.txt"
+source_file = st.file_uploader("共有したいファイルを洗濯してください")
+bucket_name = "qrdrop"
 
-# The destination bucket and filename on the MinIO server
-bucket_name = "python-test-bucket"
-destination_file = "my-test-file.txt"
+if source_file is not None:
+    with st.spinner('QRコードを生成しています…'):
+        st.info(source_file.name)
 
+        destination_file = str(uuid.uuid4()) + source_file.name
+        client.put_object(
+            bucket_name, destination_file, source_file,length=-1, part_size=1024*1024*100
+        )
+        image_url = "https://minio.launchpencil.f5.si/qrdrop/" + destination_file
+        print("saved as " + image_url)
 
-# Upload the file, renaming it in the process
-client.fput_object(
-    bucket_name, destination_file, source_file,
-)
-print(
-    source_file, "successfully uploaded as object",
-    destination_file, "to bucket", bucket_name,
-)
+        qr = qrcode.make(image_url)
+
+        image_bytes = io.BytesIO()
+        qr.save(image_bytes, format='JPEG')
+        st.image(image_bytes)
+        st.info("QRコードの生成が完了しました！")
